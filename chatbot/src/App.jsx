89df520a86a013069969bf1eb2001f774cf84f5c
PranshuChauhan149 <pranshuchauhan149@gmail.com>
2025-11-18 23:59:48 +0530

    done chatbot

--- chatbot/src/App.jsx
+++ chatbot/src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import { GoogleGenAI } from "@google/genai";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [question, setQuestion] = useState("");
+  const [res, setRes] = useState("");
+  const [loading, setLoading] = useState(false);
+  const ai = new GoogleGenAI({
+    apiKey: "AIzaSyAA0QapnuQSat6xdjVL9FVlAIKKjxldHu4",
+  });
+
+  async function main() {
+    setLoading(true);
+    const response = await ai.models.generateContent({
+      model: "gemini-2.5-flash",
+      contents: `${question}`,
+    });
+    setLoading(false);
+    setRes(response.text);
+    console.log(response.text);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <input
+        type="text"
+        onChange={(e) => setQuestion(e.target.value)}
+        required
+      />
+      <button onClick={main}>click</button>
+      <br />
+      <p>{loading ? "loading..." : ""}</p>
+      <br />
+      <pre>{res}</pre>
+    </div>
+  );
+};
 
-export default App
+export default App;
